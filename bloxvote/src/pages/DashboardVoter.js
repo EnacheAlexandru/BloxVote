a58dc5b3a38ae22542e552f81cf5598d15b90ddd
Enaa7 <enachealexandru00@yahoo.com>
2022-03-24 20:28:59 +0200

    Changed to reducers, added filters, added footer

--- bloxvote/src/pages/DashboardVoter.js
+++ bloxvote/src/pages/DashboardVoter.js
@@ -1,4 +1,4 @@
-import React, { useState, createRef, useEffect } from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import CustomButton from "../components/CustomButton";
 import logo from "../assets/logo.svg";
 import "./dashboard_voter.css";
@@ -7,136 +7,234 @@ import { Election, ElectionStatus, VoterStatus } from "../domain/Election";
 import "../utils/global.css";
 import ElectionList from "../components/ElectionList";
 import CustomButtonStatus from "../components/CustomButtonStatus";
-import { dateToString, deepCopy } from "../utils/utils";
+
+const ACTIONS = {
+  PREVIOUS_PAGE: "PREVIOUS_PAGE",
+  NEXT_PAGE: "NEXT_PAGE",
+  UPDATE_PAGINATED_ELECTIONS: "UPDATE_PAGINATED_ELECTIONS",
+  FETCH_ELECTIONS: "FETCH_ELECTIONS",
+  SET_FILTER_TITLE_VALUE: "SET_FILTER_TITLE_VALUE",
+  SET_FILTER_DATE_VALUE: "SET_FILTER_DATE_VALUE",
+  SET_NUMBER_PAGES: "SET_NUMBER_PAGES",
+  RESET_PAGINATION: "RESET_PAGINATION",
+};
+
+const itemsPerPage = 4;
 
 export default function DashboardVoter() {
+  const computeNumberPages = (list) => {
+    let numberPages;
+    if (list.length % itemsPerPage !== 0) {
+      numberPages = Math.floor(list.length / itemsPerPage) + 1;
+    } else {
+      numberPages = Math.floor(list.length / itemsPerPage);
+    }
+    return numberPages;
+  };
+
+  const stateReducer = (state, action) => {
+    switch (action.type) {
+      case ACTIONS.PREVIOUS_PAGE:
+        return {
+          ...state,
+          positionStartElections: state.positionStartElections - itemsPerPage,
+          positionEndElections: state.positionEndElections - itemsPerPage,
+          currentPage: state.currentPage - 1,
+        };
+
+      case ACTIONS.NEXT_PAGE:
+        return {
+          ...state,
+          positionStartElections: state.positionStartElections + itemsPerPage,
+          positionEndElections: state.positionEndElections + itemsPerPage,
+          currentPage: state.currentPage + 1,
+        };
+
+      case ACTIONS.UPDATE_PAGINATED_ELECTIONS: {
+        let filtered = [...state.elections];
+        if (state.filterTitleValue) {
+          filtered = filtered.filter((election) =>
+            election.title
+              .toLowerCase()
+              .includes(state.filterTitleValue.toLowerCase())
+          );
+        }
+        if (state.filterDateValue) {
+          filtered = filtered.filter(
+            (election) =>
+              state.filterDateValue >= election.dateStart &&
+              state.filterDateValue <= election.dateEnd
+          );
+        }
+        return {
+          ...state,
+          numberPages: computeNumberPages(filtered),
+          paginatedElections: filtered.slice(
+            state.positionStartElections,
+            state.positionEndElections
+          ),
+        };
+      }
+
+      case ACTIONS.FETCH_ELECTIONS:
+        return {
+          ...state,
+          elections: action.payload,
+        };
+
+      case ACTIONS.SET_FILTER_TITLE_VALUE:
+        return {
+          ...state,
+          filterTitleValue: action.payload,
+        };
+
+      case ACTIONS.SET_FILTER_DATE_VALUE:
+        return {
+          ...state,
+          filterDateValue: action.payload,
+        };
+
+      case ACTIONS.SET_NUMBER_PAGES:
+        return {
+          ...state,
+          numberPages: action.payload,
+        };
+
+      case ACTIONS.RESET_PAGINATION:
+        return {
+          ...state,
+          positionStartElections: 0,
+          positionEndElections: itemsPerPage,
+          currentPage: 1,
+        };
+    }
+  };
+
+  const initialState = {
+    positionStartElections: 0,
+    positionEndElections: itemsPerPage,
+    currentPage: 1,
+    numberPages: 0,
+    elections: [],
+    paginatedElections: [],
+    filterTitleValue: "",
+    filterDateValue: "",
+  };
+
+  const [state, stateDispatch] = useReducer(stateReducer, initialState);
+
   const [account, setAccount] = useState(
     "0xbfc06bd91802ceccefdac434412a56be26e501d4"
   );
-  const [searchValue, setSearchValue] = useState("");
-  const [dateValue, setDateValue] = useState("");
-
-  const [positionStartElections, setPositionStartElections] = useState(0);
-  const [positionEndElections, setPositionEndElections] = useState(3);
-
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const pageNumberFocus = createRef(null);
-
-  const elections = [
-    new Election(
-      1,
-      "Vote for your mayor",
-      new Date(2022, 2, 25),
-      new Date(2022, 2, 27),
-      ElectionStatus.OPEN,
-      VoterStatus.NOT_REGISTERED
-    ),
-    new Election(
-      2,
-      "Vote for your president 2022",
-      new Date(2022, 3, 5),
-      new Date(2022, 3, 7),
-      ElectionStatus.NOT_STARTED,
-      VoterStatus.NOT_REGISTERED
-    ),
-    new Election(
-      3,
-      "Vote for the new law",
-      new Date(2022, 2, 15),
-      new Date(2022, 2, 17),
-      ElectionStatus.ENDED,
-      VoterStatus.VOTED
-    ),
-    new Election(
-      4,
-      "Vote for your president 4018",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      5,
-      "Vote for your president 5010",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      6,
-      "Vote for your president 6011",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      7,
-      "Vote for your president 7012",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      8,
-      "Vote for your president 8013",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      9,
-      "Vote for your president 9013",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-    new Election(
-      10,
-      "Vote for your president 10013",
-      new Date(2018, 7, 8),
-      new Date(2018, 7, 6),
-      ElectionStatus.ENDED,
-      VoterStatus.NOT_VOTED
-    ),
-  ];
-
-  const [paginatedElections, setPaginatedElections] = useState(() => {
-    const temp = [...elections];
-    return temp.slice(positionStartElections, positionEndElections);
-  });
 
   useEffect(() => {
-    setPaginatedElections(() => {
-      const temp = [...elections];
-      return temp.slice(positionStartElections, positionEndElections);
+    stateDispatch({
+      type: ACTIONS.SET_NUMBER_PAGES,
+      payload: computeNumberPages(state.elections),
     });
-  }, [positionEndElections]);
+    stateDispatch({ type: ACTIONS.UPDATE_PAGINATED_ELECTIONS });
+  }, [state.elections]);
 
   useEffect(() => {
-    handlePageNumberFocus();
-  }, [paginatedElections]);
-
-  const handlePageNumberFocus = () =>
-    window.scrollTo({
-      top: pageNumberFocus.current.offsetTop,
+    let fetchedElections = [
+      new Election(
+        1,
+        "Vote for your mayor",
+        new Date(2022, 2, 25),
+        new Date(2022, 2, 27),
+        ElectionStatus.OPEN,
+        VoterStatus.NOT_REGISTERED
+      ),
+      new Election(
+        2,
+        "Vote for your president 2022",
+        new Date(2022, 3, 5),
+        new Date(2022, 3, 7),
+        ElectionStatus.NOT_STARTED,
+        VoterStatus.NOT_REGISTERED
+      ),
+      new Election(
+        3,
+        "Vote for the new law",
+        new Date(2022, 2, 15),
+        new Date(2022, 2, 17),
+        ElectionStatus.ENDED,
+        VoterStatus.VOTED
+      ),
+      new Election(
+        4,
+        "Vote for your president 4018",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        5,
+        "Vote for your president 5010",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        6,
+        "Vote for your president 6011",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        7,
+        "Vote for your president 7012",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        8,
+        "Vote for your president 8013",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        9,
+        "Vote for your president 9013",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+      new Election(
+        10,
+        "Vote for your president 10013",
+        new Date(2018, 7, 8),
+        new Date(2018, 7, 6),
+        ElectionStatus.ENDED,
+        VoterStatus.NOT_VOTED
+      ),
+    ];
+    stateDispatch({
+      type: ACTIONS.FETCH_ELECTIONS,
+      payload: fetchedElections.sort((a, b) => b.dateStart - a.dateStart),
     });
+  }, []);
 
   let backPageButton;
-  if (currentPage > 1) {
+  if (state.currentPage > 1) {
     backPageButton = (
       <div>
         <CustomButton
           buttonSize={"btn-size-normal"}
           onClick={() => {
-            setCurrentPage((prev) => prev - 1);
-            setPositionStartElections((prev) => prev - 3);
-            setPositionEndElections((prev) => prev - 3);
+            stateDispatch({ type: ACTIONS.PREVIOUS_PAGE });
+            stateDispatch({
+              type: ACTIONS.UPDATE_PAGINATED_ELECTIONS,
+            });
           }}
         >
           {"<"}
@@ -146,20 +244,16 @@ export default function DashboardVoter() {
   }
 
   let forwardPageButton;
-  if (
-    (elections.length % 3 !== 0 &&
-      currentPage < Math.floor(elections.length / 3) + 1) ||
-    (elections.length % 3 === 0 &&
-      currentPage < Math.floor(elections.length / 3))
-  ) {
+  if (state.currentPage < state.numberPages) {
     forwardPageButton = (
       <div>
         <CustomButton
           buttonSize={"btn-size-normal"}
           onClick={() => {
-            setCurrentPage((prev) => prev + 1);
-            setPositionStartElections((prev) => prev + 3);
-            setPositionEndElections((prev) => prev + 3);
+            stateDispatch({ type: ACTIONS.NEXT_PAGE });
+            stateDispatch({
+              type: ACTIONS.UPDATE_PAGINATED_ELECTIONS,
+            });
           }}
         >
           {">"}
@@ -169,7 +263,7 @@ export default function DashboardVoter() {
   }
 
   let noElectionsInfo;
-  if (paginatedElections.length === 0) {
+  if (state.paginatedElections.length === 0) {
     noElectionsInfo = (
       <div
         className="default-text size-large color3"
@@ -219,7 +313,12 @@ export default function DashboardVoter() {
           <CustomTextField
             width="80%"
             icon="search"
-            onChange={setSearchValue}
+            onChange={(value) =>
+              stateDispatch({
+                type: ACTIONS.SET_FILTER_TITLE_VALUE,
+                payload: value,
+              })
+            }
           ></CustomTextField>
         </div>
         <div style={{ flex: 1 }}>
@@ -232,30 +331,61 @@ export default function DashboardVoter() {
           <CustomTextField
             width="100%"
             icon="date"
-            onChange={setDateValue}
+            onChange={(value) =>
+              stateDispatch({
+                type: ACTIONS.SET_FILTER_DATE_VALUE,
+                payload: value,
+              })
+            }
           ></CustomTextField>
         </div>
       </div>
 
       <div style={{ textAlign: "center" }}>
-        <CustomButton buttonSize={"btn-size-large"}>FILTER</CustomButton>
+        <CustomButton
+          buttonSize={"btn-size-large"}
+          onClick={() => {
+            stateDispatch({
+              type: ACTIONS.RESET_PAGINATION,
+            });
+            stateDispatch({
+              type: ACTIONS.UPDATE_PAGINATED_ELECTIONS,
+            });
+          }}
+        >
+          FILTER
+        </CustomButton>
       </div>
 
       {noElectionsInfo}
 
-      <ElectionList elections={paginatedElections}></ElectionList>
+      <ElectionList elections={state.paginatedElections}></ElectionList>
 
       <div className="page-buttons">
         {backPageButton}
-        <div style={{ margin: "0 0.5% 0 0.5%" }} ref={pageNumberFocus}>
+        <div style={{ margin: "0 0.5% 0 0.5%" }}>
           <CustomButtonStatus
             buttonSize={"btn-size-normal"}
             buttonStyle={"btn-status-color3"}
           >
-            {currentPage}
+            {state.currentPage}
           </CustomButtonStatus>
         </div>
         {forwardPageButton}
+      </div>
+
+      <div className="custom-shape-divider-bottom-1648118824">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M892.25 114.72L0 0 0 120 1200 120 1200 0 892.25 114.72z"
+            className="shape-fill"
+          ></path>
+        </svg>
       </div>
     </div>
   );
